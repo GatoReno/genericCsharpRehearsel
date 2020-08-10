cdb34a1523a450264cee166fe540bd374dca47ca
OniiOphaim☠👻 <erp.reno@hotmail.com>
2020-08-10 16:26:17 -0500

    update

--- GenericsRehearsel/Program.cs
+++ GenericsRehearsel/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GenericsRehearsel
 {
@@ -6,7 +7,62 @@ namespace GenericsRehearsel
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Animals! \n ----- \n");
+
+            List<Animal> animalList = new List<Animal>();
+            animalList.Add(new Animal(){ Name = "Dragon" });
+            animalList.Add(new Animal(){ Name = "Cat" });
+            animalList.Add(new Animal(){ Name = "Crow" });
+
+            animalList.Insert(0, new Animal() { Name = "Turtle" });
+
+            foreach (var item in animalList)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            int x = 5, y = 4;
+            Animal.GetSum(ref x, ref y);
+            string stX = "4", stY = "5";
+            Animal.GetSum(ref stX, ref stY);
+
+            //Stack<T> st = new Stack<T>();
+
+            Rectangle<int> rec1 = new Rectangle<int>(2,5);
+
+            Console.WriteLine(rec1.GetArea());
+
+            Console.ReadLine();
+        }
+
+        // T refers to the Generic Class
+        public struct Rectangle<T> {
+            private T width;
+            private T length;
+            public T Width { 
+                get { return width; }
+                set { width = value; }
+            }
+
+            public T Length
+            {
+                get { return length; }
+                set { length = value; }
+            }
+
+            public Rectangle(T w , T l) {
+                width = w;
+                length = l;
+            }
+
+            public string GetArea()
+            {
+                double dblWidth = Convert.ToDouble(Width);
+                double dblLength = Convert.ToDouble(Length);
+
+                double res = dblLength * dblWidth;
+                return "Rectangle Area : "+res.ToString();
+            }
         }
     }
 }
